1799dde68b283692c93b14ff8a26374df21150cf
Houda20-bh <bh.houda14@gmail.com>
2022-11-25 23:04:19 +0100

    add my new update

--- DOM Project/main.js
+++ DOM Project/main.js
@@ -1,9 +1,10 @@
-let plus=document.querySelector("#plus");
-let minus=document.querySelector("#minus");
+
 var totalprice = document.getElementById("totalp");
 var sum = 0;
 console.log("hello");// to check 
-plus.addEventListener("click",function plus(id) {
+// adding function
+function adding(id) {
+  console.log(id);
     let quantity = document.querySelector("#q"+id[1]);
     quantity.innerHTML = parseInt(quantity.innerHTML) + 1;
     let total = document.getElementById("t"+id[1]);
@@ -11,9 +12,10 @@ plus.addEventListener("click",function plus(id) {
     total.innerHTML = parseInt(total.innerHTML) + parseInt(price.innerHTML);
     sum = parseInt(price.innerHTML) + sum;
     totalprice.innerHTML = sum;
-  });
-  minus.addEventListener("click",function  minus(id) {
-  
+  }
+  // substration function
+function  substract(id) {
+  console.log(id);
     let quantity = document.querySelector("#q"+id[1]);
     if(parseInt(quantity.innerHTML)> 0){
     quantity.innerHTML = parseInt(quantity.innerHTML) - 1;
@@ -22,5 +24,20 @@ plus.addEventListener("click",function plus(id) {
     total.innerHTML = parseInt(total.innerHTML) - parseInt(price.innerHTML);
     sum =sum- parseInt(price.innerHTML);
     totalprice.innerHTML = sum;}
-  });
+  }
 
+// remove items
+let delB = document.getElementsByClassName("delete-btn");
+for (let i = 0; i <  delB.length; i++) {
+  delB[i].onclick = function () {
+    delB[i].parentElement.remove();
+  };
+}
+// Heart
+let heartBtn = document.getElementsByClassName("fas fa-heart");
+
+for (let i = 0; i < heartBtn.length; i++) {
+  heartBtn[i].onclick = function () {
+    heartBtn[i].classList.toggle("pink");
+  };
+}
